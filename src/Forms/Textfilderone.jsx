73eadd6a4ab7a99fb6e5fda266c819124c3ab85a
Carlos Miguel Bissoni Ribeiro <bissoniribeirocarlosmiguel@gmail.com>
2023-08-09 11:20:31 -0300

    Update center formStyle

--- src/Forms/Textfilderone.jsx
+++ src/Forms/Textfilderone.jsx
@@ -5,40 +5,42 @@ import Button from '@mui/material/Button';
 
 const AppTitle = () => {
   const titleStyle = {
-    fontSize: '24px', // Tamanho da fonte desejado (por exemplo, 24px)
+    fontSize: '45px', // Tamanho da fonte desejado (por exemplo, 24px)
     fontWeight: 'bold', // Pode definir a fonte em negrito se quiser
     marginBottom: '10px', // Espaçamento inferior entre o título e o formulário (por exemplo, 10px)
+    textAlign: 'center', // Centraliza o título
   };
   return (
     <>
-    <div style={titleStyle} >Calcular IMC Seu:</div>
-    <Stack
-      component="form"
-      sx={{
-        width: '25ch',
-        marginTop: '1rem', // Espaçamento de 1 rem (ou outro valor) acima do formulário
-      }}
-      spacing={5}
-      noValidate
-      autoComplete="off"
-    >
-      <TextField
-        hiddenLabel
-        id="filled-hidden-label-normal"
-        defaultValue="Peso"
-        variant="filled"
-      />
-      <TextField
-        hiddenLabel
-        id="filled-hidden-label-normal"
-        defaultValue="Altura"
-        variant="filled"
-      />
-      <Button variant="Contained">Calcular IMC </Button>
-    </Stack>
-
-
-  </>
+      <div style={titleStyle}>Calcular IMC Seu:</div>
+      <Stack
+        component="form"
+        sx={{
+          width: '100%', // Largura máxima do formulário
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center', // Centraliza o conteúdo verticalmente
+          marginTop: '1rem', // Espaçamento de 1 rem (ou outro valor) acima do formulário
+        }}
+        spacing={2} // Espaçamento entre os campos
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          hiddenLabel
+          id="filled-hidden-label-normal"
+          defaultValue="Peso"
+          variant="filled"
+        />
+        <TextField
+          hiddenLabel
+          id="filled-hidden-label-normal"
+          defaultValue="Altura"
+          variant="filled"
+        />
+        <Button variant="contained">Calcular IMC</Button>
+      </Stack>
+    </>
   );
 };
 
